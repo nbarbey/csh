6f702235488d16d644e432350c1af95b8ba593f2
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-06-14 11:15:39 +0200

    setup.py for csh package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import Extension, setup
+setup(name='Csh',
+      version='1.0',
+      description='Compressive Sensing for Herschel',
+      author='Nicolas Barbey',
+      author_email='nicolas.barbey@cea.fr',
+      install_requires = ['numpy>=1.3.0', 'fht', ],
+      packages=['csh'],
+      )
