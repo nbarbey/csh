0b5ff74c7cef3deef6a66549cc9d8a5b8bd3b187
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2010-06-21 16:57:33 +0200

    Tests for noise filter

--- tests/test_tamasis_noise.py
+++ tests/test_tamasis_noise.py
@@ -3,6 +3,8 @@ import numpy as np
 import tamasis as tm
 import lo
 import csh.filter as filt
+from time import time
+import scipy.sparse.linalg as spl
 
 # data
 pacs = tm.PacsObservation(filename=tm.tamasis_dir+'tests/frames_blue.fits')
@@ -15,16 +17,17 @@ backmap = model.transpose(tod)
 P = lo.aslinearoperator(model.aslinearoperator())
 # derive filter
 kernel = filt.kernel_from_tod(tod, length=10)
-N = filt.noise_filter(tod.shape, kernel)
+#kern = np.mean(kernel, axis=0)
+N = filt.kernels_convolve(tod.shape, 1 / np.sqrt(kernel))
 # apply to data
-yn = N.T * tod.flatten()
+yn = N * tod.flatten()
 # apply to model
-M = P * N
+M = N * P
 # priors
 Ds = [lo.diff(backmap.shape, axis=axis) for axis in xrange(backmap.ndim)]
-Ds.append(lo.pywt_lo.wavelet2(backmap.shape, "haar"))
+#Ds.append(lo.pywt_lo.wavelet2(backmap.shape, "haar"))
 # inversion
-y = tod.flatten()
-x, conv = lo.rls(M, Ds, (1e1, 1e1, 1e-1),  y)
+#y = tod.flatten()
+x, conv = lo.rls(M, Ds, (1e1, 1e1, 1e-1),  yn, spl.bicgstab)
 sol = backmap.zeros(backmap.shape)
 sol[:] = x.reshape(sol.shape)
