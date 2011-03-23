9893edb1dcacde41439c7d5a09bfabbda0916114
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-03-23 13:54:00 +0100

    Commented test script with huber optimization.

--- tests/test_tamasis_huber.py
+++ tests/test_tamasis_huber.py
@@ -1,30 +1,62 @@
 #!/usr/bin/env python
-import numpy as np
-from copy import copy
-from tamasis import *
-import lo
+"""
+This script show how one can build on TAMASIS and use home-made
+estimation tools.
+"""
+# Imports
+# --------
+import os # to handle files
+import numpy as np # numerical computations
+from copy import copy # copy of objects
+from tamasis import * # import everything from tamasis
+import lo # my Linear Operators
 
-# data
-pacs = PacsObservation(filename=tamasis_dir+'tests/frames_blue.fits',
-                       fine_sampling_factor=1, 
-                       keep_bad_detectors=False)
+# loading data
+# -------------
+filename = os.path.join(os.getenv('HOME'), 'data', 'pacs', 'frames_blue.fits')
+# define a PacsObservation from data fits file
+# this is level 1 data in HIPE format
+pacs = PacsObservation(filename=filename, policy_bad_detector="keep")
+# this method actually loads the data in memory as a Tod
+# which is an ndarray subclass
 tod = pacs.get_tod()
-# projector
-projection = Projection(pacs, resolution=3.2, oversampling=False, npixels_per_sample=6)
-model = projection
-# naive map
-backmap = model.transpose(tod)
-# transform to lo
-P = lo.aslinearoperator(model.aslinearoperator())
-# priors
-Ds = [lo.diff(backmap.shape, axis=axis) for axis in xrange(backmap.ndim)]
-Ds.append(lo.pywt_lo.wavelet2(backmap.shape, "haar"))
-# inversion
-y = tod.flatten()
-x0 = lo.iterative.acg(P, Ds, (1e1, 1e1, 1e1), y)
-sol0 = backmap.zeros(backmap.shape)
-sol0[:] = x0.reshape(sol0.shape)
 
-x = lo.iterative.hacg(P, Ds, (1e3, 1e3, 1e3), (1e6, 1e-6, 1e-6, 1e-6), y, x0=copy(x0))
-sol = backmap.zeros(backmap.shape)
-sol[:] = x.reshape(sol.shape)
+# Generating acquisition model
+# -----------------------------
+# projector
+projection = Projection(pacs, # the PacsObseravtion object
+                        resolution=3.2, # resolution of the sky map in arcsec
+                        oversampling=False)
+# backprojection
+backmap = projection.T(tod)
+# coverage map
+coverage = projection.T(np.ones(tod.shape))
+# naive map
+naive = backmap / coverage
+# mask according to coverage (everything that is covered by less than 10.)
+mask = Masking(coverage < 10.)
+# The model is the masking of the sky map then the projection
+# This is basically matrix multiplication
+model = projection * mask
+
+# Performing inversion
+# ---------------------
+
+#  with TAMASIS
+x_tm = mapper_rls(tod, model, hyper=1e-1, tol=1e-10, maxiter=100)
+
+#  with lo routines
+# transform to lo
+H = lo.aslinearoperator(model * mask)
+# smoothness priors
+Ds = [lo.diff(backmap.shape, axis=axis) for axis in (0, 1)]
+# inversion
+y = tod.ravel() # requires 1d input
+x_lo = lo.acg(H, y, Ds, 1e-1 * np.ones(3), tol=1e-10, maxiter=100)
+x_lo.resize(backmap.shape) # output is 1d so need reshaping
+
+# with sparsity assumptions (using Huber algorithm)
+x_h = lo.hacg(H, y, Ds, 1e1 * np.ones(3),
+            np.asarray((None, 1e-6, 1e-6, 1e-6)),
+            x0=x_lo.flatten(), tol=1e-7, maxiter=200)
+x_h.resize(backmap.shape)
